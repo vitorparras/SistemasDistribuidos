83e06a56be0a6f72b441e663e2a81bc579b808b6
VITOR PARRAS <vitorhugoparras@hotmail.com>
2020-03-19 23:43:37 -0300

    base

---
+++ ExerciciosThreads/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExerciciosThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
