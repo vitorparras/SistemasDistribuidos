8e71f961df4308234aa86bc7dbc71313e6cb841f
VITOR PARRAS <vitorhugoparras@hotmail.com>
2020-03-21 14:37:25 -0300

    adicionando projetos sockets

---
+++ ExerciciosSocketsCliente/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExerciciosSocketsCliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
